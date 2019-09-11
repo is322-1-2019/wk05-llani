7b50b9985680205079400a9a7e2c117dc34f4421
nattaln <natta.ekmaruken@gmail.com>
2019-09-11 16:02:41 +0700

    week 05 homework

---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <button v-on:click="countFunction">You clicked me {{ count }} times</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonCounter',
+  data: function() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    countFunction: function() {
+      this.count++;
+    },
+    countByTwo: function() {
+      this.count+=2;
+    },
+    countReverse: function() {
+      this.count--;
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
